919e27c9800285a1560b4ba3b3457cb91a04171b
Trio <triosaputra308@gmail.com>
2020-09-07 22:13:33 +0700

    new updated

---
+++ app/src/main/java/id/co/mdd/dapoeremak/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package id.co.mdd.dapoeremak.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.co.mdd.dapoeremak.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/id/co/mdd/dapoeremak/activities/SplashActivity.java
@@ -0,0 +1,16 @@
+package id.co.mdd.dapoeremak.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.co.mdd.dapoeremak.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
