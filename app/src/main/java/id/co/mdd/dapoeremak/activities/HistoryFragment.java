6386138bf22a704810f16c62aa2d4116322f74fb
Trio <triosaputra308@gmail.com>
2020-09-20 12:05:19 +0700

    adding bottom nav

---
+++ app/src/main/java/id/co/mdd/dapoeremak/activities/HistoryFragment.java
@@ -0,0 +1,30 @@
+package id.co.mdd.dapoeremak.activities;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.co.mdd.dapoeremak.R;
+
+public class HistoryFragment extends Fragment {
+
+
+    public HistoryFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_history, container, false);
+    }
+}
--- app/src/main/java/id/co/mdd/dapoeremak/activities/MainActivity.java
+++ app/src/main/java/id/co/mdd/dapoeremak/activities/MainActivity.java
@@ -1,16 +1,44 @@
 package id.co.mdd.dapoeremak.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import id.co.mdd.dapoeremak.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.home)
+    Menu btnHome;
+
+    @BindView(R.id.history)
+    Menu btnHistory;
+
+    @BindView(R.id.profile)
+    Menu btnProfile;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        initFragment(new HomeFragment());
+
+    }
+
+    private void initFragment(Fragment classFragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragmentLayout, classFragment);
+        transaction.commit();
+
     }
 }
